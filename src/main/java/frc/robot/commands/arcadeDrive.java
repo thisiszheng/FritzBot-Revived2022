90a81518a766bd6513adb94bb62f8d277b22f63f
thisiszheng <brianz12@nycstudents.net>
2022-04-01 15:14:01 -0400

    Enter

--- src/main/java/frc/robot/commands/arcadeDrive.java
+++ src/main/java/frc/robot/commands/arcadeDrive.java
@@ -1,13 +1,16 @@
 package frc.robot.commands;
 import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
 
-public class arcadeDrive {
-    // void addRequirements(RobotContainer.m_drivetrain);
+public class arcadeDrive extends CommandBase{
+
+    public arcadeDrive(){
+        addRequirements(RobotContainer.m_drivetrain);
+        double rightSide = -RobotContainer.driverController.getRawAxis(Constants.DRIVER_CONTROLLER_MOVE_AXIS);
+        double leftSide = RobotContainer.driverController.getRawAxis(Constants.DRIVER_CONTROLLER_ROTATE_AXIS);
+        RobotContainer.m_drivetrain.arcadeDrive(1, 0, 1);
+    }
     
-double moveSpeed = -RobotContainer.driverController.getRawAxis(Constants.DRIVER_CONTROLLER_MOVE_AXIS);
-double rotateSpeed = RobotContainer.driverController.getRawAxis(Constants.DRIVER_CONTROLLER_ROTATE_AXIS);
-    
-// RobotContainer.m_drivetrain.arcadeDrive(0, 0);
 }
